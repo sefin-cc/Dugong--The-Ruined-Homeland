8af1f3acee92e6350acfc30c5f910b700bbdb384
sefin-cc <rogenasefin6@gmail.com>
2022-08-30 16:37:16 +0800

    Player Controls

---
+++ Dugong - The Ruined Homeland/Assets/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+private Vector3 direction;
+public float gravity = -8.8f;
+public float strength = 5f;
+public float tilt = 4f;
+
+
+
+private void Update(){
+    if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+        direction = Vector3.up * strength;
+    }
+    if(Input.touchCount > 0){
+        Touch touch = Input.GetTouch(0);
+
+        if(touch.phase == TouchPhase.Began){
+            direction = Vector3.up * strength;
+        }
+
+    }
+ // Apply gravity and update the position
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+
+// Tilt the bird based on the direction
+        Vector3 rotation = transform.eulerAngles;
+        rotation.z = direction.y * tilt;
+        transform.eulerAngles = rotation;
+}
+
+
+}
