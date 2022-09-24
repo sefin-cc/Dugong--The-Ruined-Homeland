d70ee3ff16d2374936e0c85f21c6c8c37b25e0b8
sefin-cc <rogenasefin6@gmail.com>
2022-09-24 21:29:55 +0800

    Display Location #2
    -  when the player completed all the level fix

--- Dugong - The Ruined Homeland/Assets/Script/InGameDisplay/locationText.cs
+++ Dugong - The Ruined Homeland/Assets/Script/InGameDisplay/locationText.cs
@@ -17,32 +17,34 @@ public class locationText : MonoBehaviour
     static int currentStartScore;
     static int currentEndScore;
     
-    public static int level = 0;
+    public static int level;
 
     // Start is called before the first frame update
     void Start()
     {
+        level = 1;
        	location = new List<Location>() { 
-                new Location(){ location = "Freedom Island", startScore = 5, endScore = 110 },
-                new Location(){ location = "Lingayen Gulf", startScore = 110, endScore = 260 }
+                new Location(){ location = "Dead Island", startScore = -5, endScore = -1 },
+                new Location(){ location = "Freedom Island", startScore = 5, endScore = 110 }
+                // new Location(){ location = "Lingayen Gulf", startScore = 110, endScore = 260 }
             };
     }
 
     void FixedUpdate()
     {
-      currentLocation = location[level].location;
-      currentStartScore = location[level].startScore;
-      currentEndScore = location[level].endScore;
+        currentLocation = location[level].location;
+        currentStartScore = location[level].startScore;
+        currentEndScore = location[level].endScore;
 
-      Debug.Log("location: "+ location.Count);
+        Debug.Log("location: "+ location.Count);
 
-      Debug.Log("currentLocation: "+currentLocation+" currentStartScore: "+currentStartScore+" currentEndScore: "+currentEndScore);
-      Debug.Log("Level: "+ level);
+        Debug.Log("currentLocation: "+currentLocation+" currentStartScore: "+currentStartScore+" currentEndScore: "+currentEndScore);
+        Debug.Log("Level: "+ level);
 
-
-      if(GameManager.score >= currentStartScore && GameManager.score < currentEndScore ){
-        StartDisplay ();
-      }
+        //Check if the player score passes the designated score  
+        if(GameManager.score >= currentStartScore && GameManager.score < currentEndScore ){
+            StartDisplay();
+        }
     }
 
 
@@ -50,12 +52,13 @@ public class locationText : MonoBehaviour
         animator.SetBool("isOpen", true);
     }
 
-
+    //This called in an animation event    
     public void DisplayNextSentence(){
         StartCoroutine(TypeSentence (currentLocation));
         StartCoroutine(waitAnimation());
     }
 
+    //Display text one by one
     IEnumerator TypeSentence (string sentence)
     {
         locText.text = "";
@@ -66,6 +69,7 @@ public class locationText : MonoBehaviour
         }
          EndDialogue();
     }
+
     IEnumerator waitAnimation()
     {
         yield return new WaitForSeconds(0.4f);
@@ -75,11 +79,14 @@ public class locationText : MonoBehaviour
     {
         if(level<location.Count-1){
             level++;
+        }else{
+            level = 0;
         }
         animator.SetBool("isOpen", false);
-        
     }
 }
+
+
 public class Location
 { 
 	public string location { get; set; }
