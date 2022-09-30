0831d091ac3d279b5f9e01cad17943bb14604fe2
sefin-cc <rogenasefin6@gmail.com>
2022-09-30 22:33:00 +0800

    Optimized BeatScore

--- Dugong - The Ruined Homeland/Assets/Script/InGameDisplay/BeatScore.cs
+++ Dugong - The Ruined Homeland/Assets/Script/InGameDisplay/BeatScore.cs
@@ -6,21 +6,38 @@ using TMPro;
 
 public class BeatScore : MonoBehaviour
 {
-     public TMP_Text beatName;
-     public TMP_Text beatScore;
-     static int playerScore;
-   
+    public TMP_Text beatName;
+    public TMP_Text beatScore;
+    static int playerScore;
 
+    string currentBeatName;
+    int currentBeatScore;
+
+    int countPlayer = 5;
+   
+    void Start(){
+        sortHighScores();
+        beatName.text = currentBeatName;
+        beatScore.text = currentBeatScore.ToString();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-       
-
-     
+            if (playerScore > currentBeatScore && countPlayer >= 0) {
+                countPlayer--;
+                sortHighScores();
+                beatName.text = currentBeatName;
+                beatScore.text = currentBeatScore.ToString();
+             
+            }
+    }
+ 
+    private void sortHighScores(){
+          
         string jsonString = PlayerPrefs.GetString("highscoreTable");
         Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
-        Debug.Log(" HighScore: "+ highscores.highscoreEntryList[0].name);
+       
 
          // Sort entry list by Score
         for (int i = 0; i < highscores.highscoreEntryList.Count; i++) {
@@ -34,25 +51,21 @@ public class BeatScore : MonoBehaviour
                 }
             }
         }
-        
-        for (int i = 0; i < highscores.highscoreEntryList.Count; i++) {
-                if (playerScore < highscores.highscoreEntryList[i].score) {
-                  beatName.text = highscores.highscoreEntryList[i].name;
-                  beatScore.text = highscores.highscoreEntryList[i].score.ToString();
-            }
-        }
-
+        currentBeatScore = highscores.highscoreEntryList[countPlayer].score;
+        currentBeatName =  highscores.highscoreEntryList[countPlayer].name;
+        Debug.Log(" currentBeatName: "+ currentBeatName);
        
     }
- 
 
     public static void GetPlayerScore(int score){
         playerScore = score;
     }
-   private class Highscores {
+
+    private class Highscores {
         public List<HighscoreEntry> highscoreEntryList;
     }
-      [System.Serializable] 
+
+    [System.Serializable] 
     private class HighscoreEntry {
         public int score;
         public string name;
