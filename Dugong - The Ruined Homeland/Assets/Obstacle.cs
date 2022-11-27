10c4d918c84eb85aec9802751bedb5d49842ff81
sefin-cc <rogenasefin6@gmail.com>
2022-11-27 18:36:06 +0800

    Score + Obstacles
    Score
    - Add points per destroy obstacle
    
    Obstacle
    - Destroy obstacle when the bubble prefab collides

--- Dugong - The Ruined Homeland/Assets/Obstacle.cs
+++ Dugong - The Ruined Homeland/Assets/Obstacle.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+   // public Animator animator;
+
+    public int currentHealth;
+    public int maxHealth = 5;
+    public int point = 5;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       currentHealth = maxHealth; 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+     //when player hits the obstacle
+    public void TakeDamage(int damage){
+        currentHealth -= damage;
+        if(currentHealth> 0){
+            //PLAY HIT ANIMATION
+            //animator.SetTrigger("damage");
+        }else{
+            //PLAY DEATH ANIMATION  
+            //animator.SetTrigger("dead");
+            destroyObstacle();
+        }
+    }
+    
+    public void destroyObstacle(){
+        FindObjectOfType<GameManager>().updateScore(point);
+        Destroy(gameObject);
     }
 }
